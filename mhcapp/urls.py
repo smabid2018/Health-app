9dd0bb5178cb72843185c69e12a7ceb0a4eac4a8
Sheikh Md Abid <smabid2018@gmail.com>
2025-02-27 00:00:52 +0545

    redirect url after login updated

--- mhcapp/urls.py
+++ mhcapp/urls.py
@@ -3,20 +3,16 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("drloa", views.drloa, name="drloa"),
-    path("loa", views.loa, name="drloa"),
-    path("admindash", views.admindash, name="admindash"),
-    path("nursedash", views.nursedash, name="nursedash"),
-    path("vitalsentry", views.vitalsentry, name="vitalsentry"),
-    path("patientsform", views.patientsform, name="patientsform"),
-    path("radiographerdash", views.radiographerdash, name="radiographerdash"),
-    path("techdash", views.techdash, name="techdash"),
-    path("radnewreq", views.radnewreq, name="radnewreq"),
-    path("patientdash", views.patientdash, name="patientdash"),
-    path("login", views.login, name="login"),
-    path("techreqdetails", views.techreqdetails, name="techreqdetails"),
-
-
-
-
+    path("drloa/", views.drloa, name="drloa"),
+    path("loa/", views.loa, name="drloa"),
+    path("admindash/", views.admindash, name="admindash"),
+    path("nursedash/", views.nursedash, name="nursedash"),
+    path("vitalsentry/", views.vitalsentry, name="vitalsentry"),
+    path("patientsform/", views.patientsform, name="patientsform"),
+    path("radiographerdash/", views.radiographerdash, name="radiographerdash"),
+    path("techdash/", views.techdash, name="techdash"),
+    path("radnewreq/", views.radnewreq, name="radnewreq"),
+    path("patient_dashboard/", views.patient_dashboard, name="patient_dashboard"),
+    path("login/", views.login_view, name="login"),
+    path("techreqdetails/", views.techreqdetails, name="techreqdetails"),
 ]
