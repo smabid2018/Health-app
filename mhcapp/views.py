1f77f165d692f41b7b43e1fd9a5beae47196c4ec
Sheikh Md Abid <smabid2018@gmail.com>
2025-03-09 03:07:19 +0545

    admin dashboard updated

--- mhcapp/views.py
+++ mhcapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth import authenticate, login 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Patient, Doctor # Add other models as needed
+from .models import Patient, Doctor, AdminProxy# Add other models as needed
 
 # Create your views here.
 
@@ -58,25 +58,51 @@ def patient_dashboard(request):
 def techreqdetails(request):
     return render(request, "techreqdetails.html")
 
+@login_required
+def doctor_dashboard(request):
+    doctor = Doctor.objects.get(user=request.user)
+    context = {
+        'user': request.user,
+        'doctor': doctor,
+        # 'upcoming_appointments': doctor.appointment_set.filter(status='scheduled'),
+        # 'patients': doctor.patients.all()
+    }
+    return render(request, 'drdash.html', context)
+
+@login_required
+def admin_dashboard(request):
+    admin = AdminProxy.objects.get(id=request.user.id)
+    context = {
+        'user': request.user,
+        'admin': admin,
+        'doctors': Doctor.objects.all(),
+        'patients': Patient.objects.all()
+    }
+    return render(request, 'admindash.html', context)
 
 def login_view(request):
     if request.method == 'POST':
         phone = request.POST.get('phone')
         password = request.POST.get('password')
+        print(f'phone: {phone}, password: {password}')
         
         # Authenticate using phone as username (since we're using phone for auth)
         user = authenticate(request, username=phone, password=password)  # Changed parameter
+        print(f'user: {user}')
         
         if user is not None:
+            print(f'user detected: {user.role}, logging in...')
             login(request, user)
             
             # Redirect based on role
             if user.role == 'patient':
-                print(f'user detected: {user.role}')
+                print(f'user detected: {user.role}, redirecting to patient_dashboard...')
                 return redirect('patient_dashboard')
             elif user.role == 'doctor':
+                print(f'user detected: {user.role}, redirecting to doctor_dashboard...')
                 return redirect('doctor_dashboard')
             elif user.role == 'admin':
+                print(f'user detected: {user.role}, redirecting to admin_dashboard...')
                 return redirect('admin_dashboard')
             # Add other roles as needed
         else:
